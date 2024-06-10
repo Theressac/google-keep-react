5a8f933467e7f4b1161b8a31b1292a633821c550
Theressac <112092897+Theressac@users.noreply.github.com>
2024-06-10 15:17:45 +0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+//import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Header</h1>
+      <h1> Create Note</h1>
+      <h1> Note</h1>
+      <h1>Footer</h1>
+
+    </div>
+  );
+}
+
+export default App;
